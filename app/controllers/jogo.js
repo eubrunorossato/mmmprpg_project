0603b02bb53994fb8bf9ad6a8be0fe5757ecc415
Bruno Rossato <brunorossato@diogos-mbp-2.br.ibm.com>
2019-06-19 13:30:37 -0300

    User is able to access the game page directly just if there is a session created

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -1,3 +1,14 @@
 module.exports.jogo = function(application, req, res){
-    res.render('jogo');
+    if(req.session.autorizado){
+        res.render('jogo');    
+    }else{
+        res.send('Usuario precisa logar primeiro')
+    }
+
+}
+
+module.exports.sair = function(application, req, res){
+    req.session.destroy( function(error){
+        res.render('index', { validacao:{} });
+    });
 }
